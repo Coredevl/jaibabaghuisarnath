edce3b9bcda31a05e526bb69277ca4f3ba08bc61
Coredevl <100237098+Coredevl@users.noreply.github.com>
2024-03-06 16:24:46 -0500

    jaibabaghuisarnath
    D:\workingdir\react\React-Landing-Page-Template-master\React-Landing-Page-Template-master\build

---
+++ precache-manifest.ca047ac73f313661fe181e83b30df92c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc617cf55c981d4f0bd8741df44748f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff2038bd8d98999093c5",
+    "url": "/static/css/main.431b0579.chunk.css"
+  },
+  {
+    "revision": "8719526ed4244ba55e4f",
+    "url": "/static/js/2.a7b4d3cc.chunk.js"
+  },
+  {
+    "revision": "ff2038bd8d98999093c5",
+    "url": "/static/js/main.b15ababc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
